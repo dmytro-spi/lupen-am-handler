7ab219c93e2e06d52177cc9880179710a9839cca
Dmytro Khekalo <dimon.messages@gmail.com>
2023-12-05 12:35:27 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,54 @@
-export { ActionMapHandler } from './actionMap/actionMapHandler';
-export { DataSchemaHandler } from './dataSchema/dataSchema';
-export type { PossibleOutput } from './actionMap/actionMapHandler'
-export type { CompatiblePaths } from './dataSchema/dataSchema'
-export type { Model } from './model/types/model'
-export * from './actionMap/types/actionMap';
-export * from './dataSchema/types/dataSchema';
+import { ActionMapHandler } from './actionMap/actionMapHandler';
+import { DataSchemaHandler } from './dataSchema/dataSchema';
+import type { PossibleOutput } from './actionMap/actionMapHandler'
+import type { CompatiblePaths } from './dataSchema/dataSchema'
+import type { Model } from './model/types/model'
+import { AccessorSources,
+    AccessorTile,ActionMap,
+    ActionTile,CONSTANT_SELECTOR,
+    ConditionalOutput,
+    DataIn,
+    ConditionOperator,
+    DefaultOutput,ForEachOutput,
+    MEMORY_SELECTOR,MODEL_SELECTOR,MemoryTile,OUTPUT_SELECTOR,Output,
+    OutputDirection,OutputGeneral,OutputType,Tile,TileType
+ } from './actionMap/types/actionMap';
+import {
+    ComplexDataType,ContentDataType,DataSchema,DataTypes,FormatDataType,SimpleDataType
+} from './dataSchema/types/dataSchema';
+
+export {
+    TileType,
+    OutputType,
+    OutputDirection,
+    ConditionOperator,
+    AccessorSources,
+    CONSTANT_SELECTOR,
+    MEMORY_SELECTOR,
+    MODEL_SELECTOR,
+    OUTPUT_SELECTOR,
+    SimpleDataType,
+    FormatDataType,
+    ActionMapHandler,
+    DataSchemaHandler,
+    ComplexDataType,
+    ContentDataType
+}
+export type {
+    Tile,
+    OutputGeneral,
+    Output,
+    MemoryTile,
+    ForEachOutput,
+    DefaultOutput,
+    DataIn,
+    ConditionalOutput,
+    ActionTile,
+    AccessorTile,
+    DataTypes,
+    DataSchema,
+    PossibleOutput,
+    CompatiblePaths,
+    Model,
+    ActionMap,
+}
